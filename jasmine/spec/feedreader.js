a7bb695faefc7e3938fed48f547a883b91f12bdb
a-yasinsky <anatoly@yasinsky.pro>
2017-09-25 10:41:42 +0600

    Add tests for URL and Name

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -26,27 +26,50 @@ $(function() {
             expect(allFeeds.length).not.toBe(0);
         });
 
-
         /* TODO: Write a test that loops through each feed
          * in the allFeeds object and ensures it has a URL defined
          * and that the URL is not empty.
          */
-
-
-        /* TODO: Write a test that loops through each feed
+		 /* TODO: Write a test that loops through each feed
          * in the allFeeds object and ensures it has a name defined
          * and that the name is not empty.
          */
+		 // function with rules to be called in loop
+		 function testURL(input) {
+			// checks URL 
+			it('URLs are defined', function() {
+				expect(input.url).toBeDefined();
+				expect(input.url.length).not.toBe(0);
+			});
+			// checks name
+			it('Names are defined', function() {
+				expect(input.name).toBeDefined();
+				expect(input.name.length).not.toBe(0);
+			});
+		 }
+		// rules calling loop
+		for(feed of allFeeds) {
+			testURL(feed);
+		}
+		
     });
 
 
     /* TODO: Write a new test suite named "The menu" */
-
-        /* TODO: Write a test that ensures the menu element is
+	describe('The menu',function(){
+		 /* TODO: Write a test that ensures the menu element is
          * hidden by default. You'll have to analyze the HTML and
          * the CSS to determine how we're performing the
          * hiding/showing of the menu element.
          */
+		it('Menu is hidden by default',function(){
+			// check that div with slide-menu class exist
+			expect();
+			// check body to have menu-hidden class
+			expect();
+		});
+	});
+       
 
          /* TODO: Write a test that ensures the menu changes
           * visibility when the menu icon is clicked. This test
